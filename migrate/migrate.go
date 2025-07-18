8e50bf3dad22392e0b8e5cbb4f5709bf2bd7d6f2
Dannyblazer <nnajidanny00@gmail.com>
2025-07-18 21:33:10 +0100

    corrected model migrations

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -14,4 +14,6 @@ func main() {
 	initializers.DB.AutoMigrate(&models.Post{})
 	initializers.DB.AutoMigrate(&models.Account{})
 	initializers.DB.AutoMigrate(&models.Wallet{})
+	initializers.DB.AutoMigrate(&models.Transfer{})
+	initializers.DB.AutoMigrate(&models.Entry{})
 }
